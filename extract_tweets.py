4520398b76f22abdcbf9c7fad13647b8c4bb3b04
Amish Goyal <amish1804@gmail.com>
2015-04-25 18:21:19 -0400

    Added Pickling

--- extract_tweets.py
+++ extract_tweets.py
@@ -1,13 +1,13 @@
 import tweepy
 import cPickle
 import time
-
-consumer_token = "BqQJc1GpaFSAEvih0cATu87Fb"
-consumer_secret = "ZpHWc4ejiZaEbLtgmcWbOapFLg3hCPvvOWWcXUNcJvTtBgU4eS"
-key = "450460896-rCRHB0dhED0aK3i60cp66hktZ7oW2rmt2vB1JoJR"
-secret = "HI8YvDJT1oxUPaq442xc8kSUw5rDo2i0T22ja0h4b3Gi7"
+from configFile import *
 
 statuses = []
+tweets = []
+count = 0
+
+pickleFile = open("data/statuses.p",'wb')
 
 def main():
 	auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
@@ -21,16 +21,20 @@ if __name__ == "__main__":
 pageIterator = tweepy.Cursor(api.search, q='#dota2').pages()
 
 while True:
-	tweets = []
 	try:
 		page = pageIterator.next()
 		for status in page:
-			statuses.append(status)
-			tweets.append(status.text)
+			if status.lang == 'en':
+				statuses.append(status)
+				tweets.append(status.text)
 	except:
+		cPickle.dump(statuses, pickleFile)
 		with open('dotaTweets.txt','a') as filename:
 			for tweet in tweets:
 				filename.write(tweet.encode('utf-8'))
 				filename.write('\n')
-		print "started sleep at: ", time.ctime()
-		time.sleep(300)
+		count += len(tweets)
+		print "started sleep at: ", time.ctime(), count
+		tweets = []
+		statuses = []
+		time.sleep(100)
