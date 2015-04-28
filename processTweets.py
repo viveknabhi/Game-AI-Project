edf007196584513151f2ba4005c1cc2eb0acd874
Amish Goyal <amish1804@gmail.com>
2015-04-28 13:57:27 -0400

    Added Process tweets module

---
+++ processTweets.py
@@ -0,0 +1,40 @@
+import cPickle
+import string
+from nltk.stem.porter import PorterStemmer as ps
+
+def tok_tweet(tweet):
+    stemmer=ps()
+    tweet = tweet.strip()
+    words = tweet.split()
+    tokenlist = []
+    exclude = set(string.punctuation)
+    punc = string.punctuation
+    punc = punc.replace('#','')
+    exclude_punc = set(punc)
+
+    for word in words:
+        word = word.strip()
+        word = word.lower()
+        #Replace URLs with @http and then with blank
+        if word.startswith('www') or word.startswith('http') or word.startswith("@") or word.isdigit():
+            continue #ignore if word is a url, @mention or contains only numbers or is a stopword
+        nword = ''.join(ch for ch in word if ch not in exclude_punc)
+        tokenlist.append(stemmer.stem(nword))
+    tokens= tokenlist
+    return ' '.join(tokens)
+
+def processStatuses():
+    corpus = ''
+    statuses = cPickle.load(open('data/statuses.p'))
+    for status in statuses:
+        if status.lang == 'en':
+            tweet = tok_tweet(status.text)
+            corpus += tweet + ' '
+    return corpus
+
+def main():
+    corpus = processStatuses()
+    return corpus
+
+if __name__ == '__main__':
+    corpus = main()
